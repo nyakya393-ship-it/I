2a6c8fbbf0b6c616f12e2045808e5dc54a8274fd
Sio <nyakya393@gmail.com>
2026-07-06 19:39:11 +0900

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,1 +1,80 @@
+// ======================================
+// Router
+// ======================================
 
+const pages = document.querySelectorAll(".page");
+
+const navLinks = document.querySelectorAll(".sidebar nav a");
+
+const pageTitle = document.getElementById("pageTitle");
+
+// ================================
+
+function showPage(pageId){
+
+    pages.forEach(page=>{
+
+        page.classList.remove("active");
+
+    });
+
+    const target = document.getElementById(pageId);
+
+    if(target){
+
+        target.classList.add("active");
+
+    }
+
+    navLinks.forEach(link=>{
+
+        link.classList.remove("active");
+
+    });
+
+    const activeLink = document.querySelector(
+
+        `.sidebar nav a[data-page="${pageId}"]`
+
+    );
+
+    if(activeLink){
+
+        activeLink.classList.add("active");
+
+    }
+
+    if(pageTitle && activeLink){
+
+        pageTitle.textContent =
+            activeLink.querySelector("span").textContent;
+
+    }
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+}
+
+// ================================
+
+navLinks.forEach(link=>{
+
+    link.addEventListener("click",()=>{
+
+        const page = link.dataset.page;
+
+        showPage(page);
+
+    });
+
+});
+
+// ================================
+
+window.showPage = showPage;
