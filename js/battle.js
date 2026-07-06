3e5175d638d731b4bf79cf564ebd65d0babdfbfb
Sio <nyakya393@gmail.com>
2026-07-06 20:19:16 +0900

    Update battle.js

--- js/battle.js
+++ js/battle.js
@@ -1,1 +1,81 @@
+// ======================================
+// Battle Data Template
+// ======================================
 
+function createBattleData(){
+
+    return{
+
+        id:Utils.createId(),
+
+        date:"",
+
+        time:"",
+
+        season:"",
+
+        mode:"",
+
+        rule:"",
+
+        stage:"",
+
+        weapon:"",
+
+        weaponImage:"",
+
+        sub:"",
+
+        subImage:"",
+
+        special:"",
+
+        specialImage:"",
+
+        result:"",
+
+        knockout:false,
+
+        rank:"",
+
+        xp:0,
+
+        kill:0,
+
+        assist:0,
+
+        death:0,
+
+        specialCount:0,
+
+        paint:0,
+
+        medal1:"",
+
+        medal2:"",
+
+        medal3:"",
+
+        gearHead:"",
+
+        gearClothes:"",
+
+        gearShoes:"",
+
+        ticket:"",
+
+        food:"",
+
+        drink:"",
+
+        image:"",
+
+        favorite:false,
+
+        note:"",
+
+        createdAt:new Date().toISOString()
+
+    };
+
+}
